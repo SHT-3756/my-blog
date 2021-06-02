49d0486e5020a7ad875885851decd5ea2ddd8df3
SHT-3756 <gkdlfndkekd@naver.com>
2021-06-02 23:24:39 +0900

    'sanity연결하기'

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,59 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import sanityClient from '@sanity/client';
 
-export default function Home() {
+export default function Home({ home, posts }) {
+  console.log('home: ',home);
+  console.log('posts: ',posts);
   return (
     <div className={styles.container}>
       <h1>Blog Home</h1>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  // sanity 로부터 데이터를 가져온다.
+  const client = sanityClient({
+    dataset: 'production',
+    projectId: '44q526eh',
+    useCdn: process.env.NODE_ENV === 'production',
+  });
+
+  const home = await client.fetch(`*[_type == 'home'][0]{'mainPostUrl': mainPost -> slug.current}`);
+
+  const posts = await client.fetch(
+    `
+      *[_type == 'post']{
+        title,
+        subtitle,
+        createdAt,
+        'content': content[]{
+          ...,
+          ...select(_type == 'imageGallery' => {'images': images[]{..., 'url' : asset -> url}})
+        },
+        'sulg': slug.current,
+        'thumbnail': {
+          'alt': thumbnail.alt,
+          'imageUrl': thumbnail.asset -> url
+        },
+        'author': author -> {
+          name, 
+          role,
+          'image': image.asset -> url
+        },
+        'tag': tag -> {
+          title,
+          'slug': slug.current
+        }
+      }
+    `
+  );
+
+
+  return {
+    props: {
+      home,
+      posts,
+    },
+  };
 }
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,12 +1,8 @@
 import React from 'react'
-import {useRouter} from "next/router";
+import sanityClient from '@sanity/client';
 
-function PostAll() {
-    const router = useRouter();
+export default function PostAll({slug}) {
 
-    const { slug } = router.query;
-
-    
     return (
         <div>
             <h1>Post: {slug}</h1>
@@ -14,4 +10,60 @@ function PostAll() {
     )
 }
 
-export default PostAll
+ export async function getStaticPaths() {
+       // sanity 로부터 데이터를 가져온다.
+  const client = sanityClient({
+    dataset: 'production',
+    projectId: '44q526eh',
+    useCdn: process.env.NODE_ENV === 'production',
+  });
+
+
+  const posts = await client.fetch(
+    `
+      *[_type == 'post']{
+        title,
+        subtitle,
+        createdAt,
+        'content': content[]{
+          ...,
+          ...select(_type == 'imageGallery' => {'images': images[]{..., 'url' : asset -> url}})
+        },
+        'sulg': slug.current,
+        'thumbnail': {
+          'alt': thumbnail.alt,
+          'imageUrl': thumbnail.asset -> url
+        },
+        'author': author -> {
+          name, 
+          role,
+          'image': image.asset -> url
+        },
+        'tag': tag -> {
+          title,
+          'slug': slug.current
+        }
+      }
+    `
+  );
+
+  const paths = posts.map((post) => ({
+      params: {
+          slug: post.slug,
+      },
+  }));
+
+     return {
+         paths,
+         fallback: false,
+     };
+}
+
+ export function getStaticProps({params}) {
+     const {slug} = params;
+     return {
+         props: {
+             slug,
+         },
+     };
+ }
